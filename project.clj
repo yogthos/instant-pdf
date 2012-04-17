11d5fbd011e0cda748d9f984b94ba7a7dedaa758
Yogthos <yogthos@gmail.com>
2012-04-16 20:08:27 -0400

    updated markdown dependency

--- project.clj
+++ project.clj
@@ -4,10 +4,14 @@
                  [compojure "1.0.2"]
                  [hiccup "1.0.0-RC2"]
                  [org.clojure/data.json "0.1.1"]
-                 [markdown-clj "0.5"]
+                 [markdown-clj "0.6"]
                  [clj-pdf "0.2.0-SNAPSHOT"]
                  [ring/ring-servlet "1.1.0-RC1"]
        		 [com.lowagie/itext "2.1.7"]]
           		 
-  :dev-dependencies [[lein-ring "0.5.0"]]
-  :ring {:handler app.routes/app})
+  :dev-dependencies [[ring "1.1.0-RC1"]
+                     [lein-ring "0.6.4"]
+                     [com.mefesto/lein-jetty "1.0.0-SNAPSHOT"]
+                     [uk.org.alienscience/leiningen-war "0.0.12"]]
+  :ring {:handler app.routes/app}
+  :jetty {:context-path "/"})
