8c9d6bcca921012f05e4b872d0cd76a8d9034641
Yogthos <dmitri.sotnikov@gmail.com>
2012-12-05 23:35:15 -0500

    updated clj-pdf dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [hiccup "1.0.1"]
                  [cheshire "4.0.3"]
                  [markdown-clj "0.9.9"]
-                 [clj-pdf "1.0.2"]
+                 [clj-pdf "1.0.5"]
                  [ring/ring-servlet "1.1.0"]                 
                  [ring/ring-jetty-adapter "1.1.0"]]
   :plugins [[lein-ring "0.7.5"]]       		 
