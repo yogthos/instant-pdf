1184731e006cd7d15bfc3d177b4f81b03b26355c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-06-09 08:19:55 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -15,6 +15,4 @@
              {:ring
               {:open-browser? false
                :stacktraces? false
-               :auto-reload? false}}
-             :dev {:dependencies [[ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.1.0"]]}})
+               :auto-reload? false}}})
