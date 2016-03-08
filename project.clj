c63bb46c26e828d7197f421dc22174c115370027
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-03-08 08:24:56 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject instant-pdf "0.2.3"
   :description "JSON to PDF service"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
                  [hiccup "1.0.5"]
                  [cheshire "5.5.0"]
-                 [markdown-clj "0.9.85"]
-                 [clj-pdf "2.1.7"]
+                 [markdown-clj "0.9.86"]
+                 [clj-pdf "2.2.0"]
                  [ring-server "0.4.0"]]
   :min-lein-version "2.0.0"
   :aot :all
